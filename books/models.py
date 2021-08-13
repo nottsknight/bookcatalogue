2652630ae731926afb546ef2e5dc939e3f38ccd7
Ian Knight <ian.knight2@nottingham.ac.uk>
2021-08-13 12:07:51 +0100

    Create Author model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from functools import total_ordering
 
 # Create your models here.
+
+
+@total_ordering
+class Author(models.Model):
+    last_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    middle_names = models.CharField(max_length=128, blank=True, null=True)
+
+    def __str__(self):
+        if self.middle_names:
+            return f'{self.last_name}, {self.first_name} {self.middle_names}'
+        else:
+            return f'{self.last_name}, {self.first_name}'
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Author):
+            return False
+
+        return ((self.last_name, self.first_name, self.middle_names) ==
+                (o.last_name, o.first_name, o.middle_names)
+                )
+
+    def __lt__(self, o):
+        if not isinstance(o, Author):
+            return False
+
+        return ((self.last_name, self.first_name, self.middle_names) <
+                (o.last_name, o.first_name, o.middle_names)
+                )
